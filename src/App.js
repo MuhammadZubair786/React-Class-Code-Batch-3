0aa074fd5a4c407221c475d68e647037de0c45d5
MuhammadZubair786/ <mzhassan444@gmail.com>
2024-04-06 13:34:25 +0500

    update

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Todo from "./Components/Todo"
+
+function App() {
+  return (
+    <div style={{textAlign:"center"}}>
+      <h1>Todo App</h1>
+      <Todo />
+    </div>
+
+  )
+}
+export default App
